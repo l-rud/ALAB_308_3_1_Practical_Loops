a2638d30bccf9164789aaa01f695eae5ff30955b
Larysa Rud <larpavrud@gmail.com>
2024-04-12 17:20:18 -0400

    Part 2: Prime Time

--- practicalLoops.js
+++ practicalLoops.js
@@ -18,4 +18,28 @@ for (let i = 1; i < 101; i++) {
     }
 }
 
+/*Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop. */
+let n = 9;
+let i;
+let prime;
+for (i=n; true; i++)
+{
+    prime = true;
+    for (j = 2; j < i; j++){
+        if (i % j == 0){
+            prime = false;
+            break;
+        }
+    }
 
+    if (prime) {
+        console.log(`Prime number is  ${i}`);
+        break;
+    }
+}
+
+
+
+    
